32244271d0774296afa212fa9590c6725c566f17
Jim McDonough <jimmc83@yahoo.com>
2023-12-26 16:46:44 -0500

    Exercise 5

--- routes/users.js
+++ routes/users.js
@@ -42,11 +42,31 @@ router.post("/",(req,res)=>{
 });
 
 
-// PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
-});
+    const email = req.params.email;
+    let filtered_users = users.filter((user) => user.email === email);
+    if (filtered_users.length > 0) {
+        let filtered_user = filtered_users[0];
+        let DOB = req.query.DOB;
+        let firstName = req.query.firstName;
+        let lastName = req.query.lastName;
+        if(DOB) {
+            filtered_user.DOB = DOB
+        }
+        if(firstName){
+            filtered_user.firstName = firstName
+        }
+        if(lastName){
+            filtered_user.lastName = lastName
+        }
+        users = users.filter((user) => user.email != email);
+        users.push(filtered_user);
+        res.send(`User with the email  ${email} updated.`);
+    }
+    else{
+        res.send("Unable to find user!");
+    }
+  });
 
 
 // DELETE request: Delete a user by email ID
